9031f4caeda5583b0f42edf013003e64076581e8
Bartek K <bkasicki754@gmail.com>
2026-03-05 00:04:38 +0100

    Dodano obliczenia matematyczne dla klasy Circle

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,3 +1,12 @@
 
 #include "Circle.h"
 
+template <class T>
+void Circle<T>::compute() {
+    T radius=this->param->get(PARAM_RADIUS);
+
+const double PI=3.14159265358979323846;
+this->area=static_cast<T>(PI*radius*radius);
+this->perimeter=static_cast<T>(2*PI*radius);
+}
+
