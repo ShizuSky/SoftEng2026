ccf5ecddd008fcb89d41c329c393cbd0ca03e361
Oskar Szafraniec <oskar.szafraniec@o2.pl>
2026-03-05 21:39:14 +0100

    Implementacja Ellipse compute i print

--- src/Ellipse.h
+++ src/Ellipse.h
@@ -1,31 +1,44 @@
 #ifndef _ELLIPSE_H
 #define _ELLIPSE_H
 
-
 #include "Shape2D.h"
 #include "ShapeResultData.h"
 #include <string>
 using namespace std;
 #include "ShapeParam.h"
+#include "ShapeParamIndex.h"
+#include "ShapeResultIndex.h"
 
 template<class T>
 class Ellipse : public Shape2D<T> {
-  public:
-    inline ShapeResultData<T> compute();
-
-    inline string print();
-
-    inline Ellipse(const ShapeParam<T> & param);
-
+    public:
+        inline ShapeResultData<T> compute();
+        inline string print();
+        inline Ellipse(const ShapeParam<T> & param);
 };
+
 template<class T>
 inline ShapeResultData<T> Ellipse<T>::compute() {
-  return ShapeResultData<T>();
+    T w = this->m_param.get(ShapeParamIndex::PARAM_WIDTH);
+    T h = this->m_param.get(ShapeParamIndex::PARAM_HEIGHT);
+
+    T a = w / (T)2;
+    T b = h / (T)2;
+
+    ShapeResultData<T> res;
+    
+    const double PI = 3.14159265358979323846;
+    res.set(ShapeResultIndex::RESULT_AREA, (T)(PI * a * b));
+    
+    return res;
 }
 
 template<class T>
 inline string Ellipse<T>::print() {
-  return "";
+    T w = this->m_param.get(ShapeParamIndex::PARAM_WIDTH);
+    T h = this->m_param.get(ShapeParamIndex::PARAM_HEIGHT);
+
+    return "Ellipse(" + to_string(w) + ", " + to_string(h) + ")";
 }
 
 template<class T>
