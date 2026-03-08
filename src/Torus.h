e96a3f4b4439660c4a2f0303149a4e746d71e52a
Julia <grazia.julia@gmail.com>
2026-03-08 10:17:37 +0100

    fix formatting

--- src/Torus.h
+++ src/Torus.h
@@ -5,29 +5,27 @@
 #include "ShapeResultData.h"
 #include <string>
 #include <cmath>
-//#include <numbers> // (C++20)
+// #include <numbers> // (C++20)
 using namespace std;
 #include "ShapeParam.h"
 
-template<class T>
-class Torus : public Shape3D<T> {
-  public:
+template <class T> class Torus : public Shape3D<T> {
+public:
     inline virtual ShapeResult<T> compute();
 
     inline string print();
 
-    inline Torus(const ShapeParam<T> & param);
-
+    inline Torus(const ShapeParam<T>& param);
 };
 
-template<class T>
-inline ShapeResult<T> Torus<T>::compute() {
+template <class T> inline ShapeResult<T> Torus<T>::compute()
+{
     ShapeResult<T> result;
 
     T R = this->m_param.get_attrib(PARAM_RADIUS);
     T r = this->m_param.get_attrib(PARAM_RADIUS_2);
 
-    //const T PI = static_cast<T>(numbers::pi); // (C++20)
+    // const T PI = static_cast<T>(numbers::pi); // (C++20)
     const T PI = static_cast<T>(M_PI);
 
     T volume = 2 * PI * PI * R * r * r;
@@ -39,16 +37,16 @@ inline ShapeResult<T> Torus<T>::compute() {
     return result;
 }
 
-template<class T>
-inline string Torus<T>::print() {
+template <class T> inline string Torus<T>::print()
+{
     T R = this->m_param.get_attrib(PARAM_RADIUS);
     T r = this->m_param.get_attrib(PARAM_RADIUS_2);
 
     return "Torus(R=" + to_string(R) + ", r=" + to_string(r) + ")";
 }
 
-template<class T>
-inline Torus<T>::Torus(const ShapeParam<T> & param) : Shape3D<T>(param) {
-}
+template <class T>
+inline Torus<T>::Torus(const ShapeParam<T>& param): Shape3D<T>(param)
+{}
 
 #endif
