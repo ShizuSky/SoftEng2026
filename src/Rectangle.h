4d23b8074a6705b6020331fcece13dc82bb02c85
Eryk Nowacki <129730@uksw.edu.pl>
2026-03-08 09:47:28 +0100

    rozwiązanie konfliktu

--- src/Rectangle.h
+++ src/Rectangle.h
@@ -1,47 +1,50 @@
 #ifndef _RECTANGLE_H
 #define _RECTANGLE_H
 
-
+#include <string>
+#include "Shape3D.h"
 #include "Shape2D.h"
 #include "ShapeResultData.h"
-#include <string>
-using namespace std;
+#include <cmath>
 #include "ShapeParam.h"
 
 template<class T>
 class Rectangle : public Shape2D<T> {
-  public:
-    inline virtual ShapeResult<T> compute();
+public:
 
-    inline string print();
+    virtual ShapeResult<T> compute() override;
 
-    inline Rectangle(const ShapeParam<T> & param);
+    std::string print();
 
+    Rectangle(const ShapeParam<T>& m_param);
 };
-template<class T>
-inline ShapeResultData<T> Rectangle<T>::compute() {
 
-    T a = this->param.getA();
-    T b = this->param.getB();
+
+template<class T>
+inline ShapeResult<T> Rectangle<T>::compute() {
+    T a = this->m_param.get_attrib(ShapeParamIndex(0));
+    T b = this->m_param.get_attrib(ShapeParamIndex(0));
 
     T area = a * b;
-    T perimeter = 2 * (a + b);
 
-    return ShapeResultData<T>(area, perimeter);
+    T perimeter = static_cast<T>(2) * (a + b);
+
+    ShapeResult<T> result;
+    result.set_attrib(ShapeResultIndex(0), area);
+    result.set_attrib(ShapeResultIndex(1), perimeter);
+
+    return result;
 }
 
 template<class T>
-inline string Rectangle<T>::print() {
-
-    T a = this->param.getA();
-    T b = this->param.getB();
-
-    return "Rectangle a=" + to_string(a) + " b=" + to_string(b);
+inline std::string Rectangle<T>::print() {
+    T a = this->m_param.get_attrib(ShapeParamIndex(0));
+    T b = this->m_param.get_attrib(ShapeParamIndex(0));
+    return "Rectangle a=" + std::to_string(a) + " b=" + std::to_string(b);
 }
 
-
 template<class T>
-inline Rectangle<T>::Rectangle(const ShapeParam<T> & param) : Shape2D<T>(param) {
+inline Rectangle<T>::Rectangle(const ShapeParam<T>& m_param) : Shape2D<T>(m_param) {
 }
 
 #endif
